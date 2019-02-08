c45f2ec0b65008476216269495f774fa2bc6b537
strawmanbobi <strawmanbobi@163.com>
2019-02-08 16:01:58 +0800

    updated java package names

---
+++ src/main/java/net/irext/decode/service/model/IRBinary.java
@@ -0,0 +1,42 @@
+package net.irext.decode.service.model;
+
+/**
+ * Filename:       IRBinary.java
+ * Revised:        Date: 2018-12-30
+ * Revision:       Revision: 1.0
+ * <p>
+ * Description:    Remote binary in cache
+ * <p>
+ * Revision log:
+ * 2018-12-30: created by strawmanbobi
+ */
+public class IRBinary {
+
+    private Integer id;
+    private byte[] binary;
+
+    public IRBinary(Integer id, byte[] binary) {
+        this.id = id;
+        this.binary = binary;
+    }
+
+    public IRBinary() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public byte[] getBinary() {
+        return binary;
+    }
+
+    public void setBinary(byte[] binary) {
+        this.binary = binary;
+    }
+}
